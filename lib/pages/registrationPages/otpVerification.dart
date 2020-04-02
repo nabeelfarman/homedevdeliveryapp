0b4f39a1f40f61dfc926054159a3668398f92e55
haroonqadeer <haroon.qadeer@hotmail.com>
2020-04-02 13:20:32 +0500

    OTP further settings

--- lib/pages/registrationPages/otpVerification.dart
+++ lib/pages/registrationPages/otpVerification.dart
@@ -50,77 +50,75 @@ class _OTPVerification extends State<OTPVerification> {
             ),
             FadeAnimation(
               1.5,
-              Container(
-                  height: 350,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Center(
-                        child: Icon(
-                          Icons.alarm,
-                          size: 80,
-                          color: blueClr,
-                        ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Center(
+                    child: Icon(
+                      Icons.alarm,
+                      size: 80,
+                      color: blueClr,
+                    ),
+                  ),
+                  Text(
+                    '3:00',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: blackClr,
+                        fontSize: 30,
+                        fontFamily: 'Josefin',
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 50, right: 50),
+                    child: Text(
+                      'One Time Password  (OTP) has been sent to your registered mobile number. Please enter  One Time Password (OTP) within the stipulated time.',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontFamily: 'Baloo', fontSize: 18),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 50, right: 50),
+                    child: TextFormField(
+                      key: Key('password'),
+                      keyboardType: TextInputType.number,
+                      textAlign: TextAlign.center,
+                      maxLength: 4,
+                      decoration: InputDecoration(
+                        hintText: '04 digit OTP',
+                        // labelText: 'mobile number',
                       ),
-                      Text(
-                        '3:00',
-                        textAlign: TextAlign.center,
+                      validator: (String value) {
+                        if (value.isEmpty) {
+                          return 'Enter Verification Code';
+                        }
+                      },
+                      onSaved: (String value) {},
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 125, right: 125),
+                    child: RaisedButton(
+                      elevation: 10,
+                      padding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                      shape: new RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(10),
+                      ),
+                      color: greenClr,
+                      splashColor: blackClr,
+                      child: Text(
+                        "Resend OTP",
                         style: TextStyle(
-                            color: blackClr,
-                            fontSize: 30,
-                            fontFamily: 'Josefin',
-                            fontWeight: FontWeight.bold),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 50, right: 50),
-                        child: Text(
-                          'One Time Password  (OTP) has been sent to your registered mobile number. Please enter  One Time Password (OTP) within the stipulated time.',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(fontFamily: 'Baloo', fontSize: 18),
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontFamily: 'Abel',
                         ),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 50, right: 50),
-                        child: TextFormField(
-                          key: Key('password'),
-                          keyboardType: TextInputType.number,
-                          textAlign: TextAlign.center,
-                          maxLength: 4,
-                          decoration: InputDecoration(
-                            hintText: '04 digit OTP',
-                            // labelText: 'mobile number',
-                          ),
-                          validator: (String value) {
-                            if (value.isEmpty) {
-                              return 'Enter Verification Code';
-                            }
-                          },
-                          onSaved: (String value) {},
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 125, right: 125),
-                        child: RaisedButton(
-                          elevation: 10,
-                          padding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-                          shape: new RoundedRectangleBorder(
-                            borderRadius: new BorderRadius.circular(10),
-                          ),
-                          color: greenClr,
-                          splashColor: blackClr,
-                          child: Text(
-                            "Resend OTP",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20,
-                              fontFamily: 'Abel',
-                            ),
-                          ),
-                          onPressed: () {},
-                        ),
-                      ),
-                    ],
-                  )),
+                      onPressed: () {},
+                    ),
+                  ),
+                ],
+              ),
             ),
             FadeAnimation(
               2.0,
@@ -153,54 +151,49 @@ class _OTPVerification extends State<OTPVerification> {
                     ),
                     onPressed: () {},
                   ),
-                  Container(
-                    width: 100,
-                    height: 30,
-                    //color: Colors.orangeAccent,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Container(
-                          width: 15,
-                          height: 15,
-                          decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              border: Border.all(color: blueClr)),
-                        ),
-                        Container(
-                          width: 15,
-                          height: 15,
-                          decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: blueClr,
-                              border: Border.all(color: blueClr)),
-                        ),
-                        Container(
-                          width: 15,
-                          height: 15,
-                          decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              //color: blueClr,
-                              border: Border.all(color: blueClr)),
-                        ),
-                        Container(
-                          width: 15,
-                          height: 15,
-                          decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              //color: blueClr,
-                              border: Border.all(color: blueClr)),
-                        ),
-                        Container(
-                          width: 15,
-                          height: 15,
-                          decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              //color: blueClr,
-                              border: Border.all(color: blueClr)),
-                        )
-                      ],
-                    ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Container(
+                        width: 15,
+                        height: 15,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            border: Border.all(color: blueClr)),
+                      ),
+                      Container(
+                        width: 15,
+                        height: 15,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: blueClr,
+                            border: Border.all(color: blueClr)),
+                      ),
+                      Container(
+                        width: 15,
+                        height: 15,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            //color: blueClr,
+                            border: Border.all(color: blueClr)),
+                      ),
+                      Container(
+                        width: 15,
+                        height: 15,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            //color: blueClr,
+                            border: Border.all(color: blueClr)),
+                      ),
+                      Container(
+                        width: 15,
+                        height: 15,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            //color: blueClr,
+                            border: Border.all(color: blueClr)),
+                      )
+                    ],
                   ),
                   FlatButton(
                     padding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
