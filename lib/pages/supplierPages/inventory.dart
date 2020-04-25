91b4fbfa4d1cdd10679fd574f4f5020cc78c524e
haroonqadeer <haroon.qadeer@hotmail.com>
2020-04-25 15:31:58 +0500

    inventory UI

--- lib/pages/supplierPages/inventory.dart
+++ lib/pages/supplierPages/inventory.dart
@@ -9,13 +9,17 @@ class InventoryPage extends StatefulWidget {
 
 class _InventoryPageState extends State<InventoryPage>
     with SingleTickerProviderStateMixin {
-  Color blackClr = Color(0xff2d2d2d);
-  // Color yellowClr = Color(0xfff7d73a);
+  Color blackClr = Color(0xff1D2028);
+
   Color whiteClr = Color(0x0ffffffff);
-  Color lightClr = Color(0x0fffdebe7);
-  Color purpleClr = Color(0x0ffd183fd);
-  Color greenClr = Color(0x0ff8ee269);
-  Color redClr = Color(0x0fff0513c);
+  Color lightClr = Color(0x0ffEEF2F5);
+  Color greyClr = Color(0x0ffB5BED0);
+  Color greenClr = Color(0x0ffA3C12E);
+  Color redClr = Color(0x0ffcf3f3d);
+
+  Color yellowClr = Color(0x0ffF8D247);
+  Color darkYellowClr = Color(0x0ffdfbd3f);
+  Color lightYellowClr = Color(0x0ffffde22);
 
   bool isSearching = false;
 
@@ -161,57 +165,62 @@ class _InventoryPageState extends State<InventoryPage>
       appBar: new AppBar(
         centerTitle: true,
         elevation: 0,
-        backgroundColor: whiteClr,
+        backgroundColor: darkYellowClr,
         title: !isSearching
             ? Text('Inventory Settings',
                 style: TextStyle(
-                    color: blackClr, fontFamily: 'Josefin', fontSize: 25))
+                    color: blackClr, fontFamily: 'Anton', fontSize: 25))
             : TextField(
                 onChanged: (Text) {
                   // supplierListPage.prinText(value);
                   _filteredItems(Text);
                   // _tabController.animateTo(_tabController.index - 1);
+                  print(1);
                 },
                 style: TextStyle(color: redClr),
                 decoration: InputDecoration(
-                    icon: Icon(
+                    prefixIcon: Icon(
                       Icons.search,
-                      color: redClr,
+                      color: blackClr.withOpacity(0.4),
                     ),
+                    fillColor: darkYellowClr,
+                    filled: true,
                     focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: redClr)),
+                        borderSide: BorderSide(color: blackClr)),
                     hintText: 'search item',
                     hintStyle: TextStyle(
                         color: redClr, fontFamily: 'Baloo', fontSize: 18))),
+        actions: <Widget>[
+          isSearching
+              ? IconButton(
+                  icon: Icon(Icons.close),
+                  onPressed: () {
+                    setState(() {
+                      this.isSearching = false;
+                    });
+                  },
+                  color: redClr)
+              : IconButton(
+                  icon: Icon(Icons.search),
+                  onPressed: () {
+                    setState(() {
+                      this.isSearching = true;
+                    });
+                  },
+                  color: redClr)
+        ],
       ),
       body: ListView(
         children: <Widget>[
-          FadeAnimation(
-            1.0,
-            Padding(
-              padding: const EdgeInsets.only(left: 20, right: 20),
-              child: TextField(
-                  onChanged: (Text) {
-                    // supplierListPage.prinText(value);
-                    _filteredItems(Text);
-                    // _tabController.animateTo(_tabController.index - 1);
-                    print(1);
-                  },
-                  style: TextStyle(color: redClr),
-                  decoration: InputDecoration(
-                      icon: Icon(
-                        Icons.search,
-                        color: redClr,
-                      ),
-                      focusedBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: redClr)),
-                      hintText: 'search item',
-                      hintStyle: TextStyle(
-                          color: redClr, fontFamily: 'Baloo', fontSize: 18))),
-            ),
-          ),
           Container(
-            height: MediaQuery.of(context).size.height - 180.0,
+            decoration: new BoxDecoration(
+              // color: yellowClr,
+              gradient: new LinearGradient(
+                  colors: [darkYellowClr, lightYellowClr, yellowClr],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter),
+            ),
+            height: MediaQuery.of(context).size.height,
             width: double.infinity,
             child: new ListView.builder(
               itemCount: filteredItems.length,
@@ -223,10 +232,14 @@ class _InventoryPageState extends State<InventoryPage>
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
-        backgroundColor: Color(0xFFF17532),
-        child: Icon(Icons.notifications),
+        backgroundColor: blackClr,
+        child: Icon(
+          Icons.notifications,
+          color: lightYellowClr,
+        ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      backgroundColor: lightYellowClr,
       bottomNavigationBar: SupplierBottomBar(),
     );
   }
@@ -236,58 +249,65 @@ class _InventoryPageState extends State<InventoryPage>
     return FadeAnimation(
       1.5,
       Card(
+          color: lightClr,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
           child: Padding(
-        padding: const EdgeInsets.all(8),
-        child: Column(
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            padding: const EdgeInsets.all(8),
+            child: Column(
               children: <Widget>[
-                Text(item['itemTitle'],
-                    style: TextStyle(
-                        color: blackClr,
-                        fontFamily: 'Baloo',
-                        fontSize: 18,
-                        fontWeight: FontWeight.w800)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(item['itemTitle'],
+                        style: TextStyle(
+                            color: blackClr,
+                            fontFamily: 'Baloo',
+                            fontSize: 18,
+                            fontWeight: FontWeight.w800)),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text('Rs.  ',
+                        style: TextStyle(
+                            color: redClr, fontFamily: 'Baloo', fontSize: 18)),
+                    Flexible(
+                        child: TextFormField(
+                      key: Key('price'),
+                      decoration: InputDecoration(
+                        hintText: 'price',
+                        focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: redClr)),
+                      ),
+                      maxLength: 5,
+                      keyboardType: TextInputType.number,
+                      textInputAction: TextInputAction.next,
+                    )),
+                    Text('  Quantity:  ',
+                        style: TextStyle(
+                            color: redClr, fontFamily: 'Baloo', fontSize: 18)),
+                    Flexible(
+                        child: TextFormField(
+                      key: Key('quantity'),
+                      decoration: InputDecoration(
+                        hintText: 'quantity',
+                        focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: redClr)),
+                      ),
+                      maxLength: 5,
+                      keyboardType: TextInputType.number,
+                      textInputAction: TextInputAction.done,
+                    )),
+                    Text('  ' + item['unit'] + '   ',
+                        style: TextStyle(
+                            color: redClr, fontFamily: 'Baloo', fontSize: 18)),
+                  ],
+                )
               ],
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text('Rs.  ',
-                    style: TextStyle(
-                        color: redClr, fontFamily: 'Baloo', fontSize: 18)),
-                Flexible(
-                    child: TextFormField(
-                  key: Key('price'),
-                  decoration: InputDecoration(
-                    hintText: 'price',
-                  ),
-                  maxLength: 5,
-                  keyboardType: TextInputType.number,
-                  textInputAction: TextInputAction.next,
-                )),
-                Text('  Quantity:  ',
-                    style: TextStyle(
-                        color: redClr, fontFamily: 'Baloo', fontSize: 18)),
-                Flexible(
-                    child: TextFormField(
-                  key: Key('quantity'),
-                  decoration: InputDecoration(
-                    hintText: 'quantity',
-                  ),
-                  maxLength: 5,
-                  keyboardType: TextInputType.number,
-                  textInputAction: TextInputAction.done,
-                )),
-                Text('  ' + item['unit'] + '   ',
-                    style: TextStyle(
-                        color: redClr, fontFamily: 'Baloo', fontSize: 18)),
-              ],
-            )
-          ],
-        ),
-      )),
+          )),
     );
   }
 }
